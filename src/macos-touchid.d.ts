1c88a6f2d7b247eb22793e5cbd1f640a8b606be9
Fareed Farag <fareedayman03@gmail.com>
2025-08-07 20:20:21 -0400

    fix macos-touchid type definition

--- src/macos-touchid.d.ts
+++ src/macos-touchid.d.ts
@@ -1,4 +1,9 @@
 declare module "macos-touchid" {
   export function canAuthenticate(): boolean;
-  export function authenticate(reason: string, callback: (error: Error | null) => void): void;
+  /**
+   * Attempts Touch ID / local auth.
+   * @param reason The user-visible reason string
+   * @param callback (error, didAuthenticate) — if error is null and didAuthenticate is false, user cancelled or did not authenticate
+   */
+  export function authenticate(reason: string, callback: (error: Error | null, didAuthenticate: boolean) => void): void;
 }
